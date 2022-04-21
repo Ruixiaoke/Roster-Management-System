72afe9472cf93e898ae77c034287dbf622b21c9b
Ruixiaoke <524373365@qq.com>
2022-04-21 11:25:41 +1000

    change nav and create login page and register page
    frame work update

---
+++ roster_management_system/lib/widge/register.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class registerpage extends StatelessWidget {
+  const registerpage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
